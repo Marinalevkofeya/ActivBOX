61cf0330995f1cfcc2106473b9478a3d5274c7c1
Maryna Kutuzava <levkofeya202@gmail.com>
2024-07-16 03:05:43 +0300

    1 all

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,11 @@
-console.log('Aloha!!!')
+import $ from 'jquery';
+import 'slick-carousel';
+
+$('.carousel').slick({
+    dots: true,
+    slidesToShow: 1, //кол. одноврименно
+    infinite: false, // отключ зацикленность
+    slidesToScroll: 1, // сколько при  клейке на скрол -?
+    fade: true, // затенялись отзыва один появлялся другой исчезал
+    arrows: true, // стрелки
+});
